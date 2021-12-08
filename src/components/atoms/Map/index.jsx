7a46a67fb2c9153a07aee1ccd36887bafb95ee7f
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 23:24:17 +0000

    index.jsx: changed

--- src/components/atoms/Map/index.jsx
+++ src/components/atoms/Map/index.jsx
@@ -3,18 +3,6 @@ import * as React from 'react';
 export default function Map(props) {
 
     return (
-        <Link
-            href={url}
-            aria-label={altText}
-            id={cssId}
-            className={classNames('sb-component', 'sb-component-block', 'sb-component-social', cssClasses, {
-                'sb-component-social-primary': style === 'primary',
-                'sb-component-social-secondary': style === 'secondary'
-            })}
-            data-sb-field-path={annotations.join(' ').trim()}
-        >
-            {label && <span className="sr-only">{label}</span>}
-            {IconComponent && <IconComponent className="fill-current h-5 w-5" />}
-        </Link>
+        
     );
 }
