6d1af173341d3cf56bb8559ab4d3dcfd9d3c32fe
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 23:25:23 +0000

    Map: added

---
+++ src/components/sections/Map/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export default function Map(props) {
+
+    return (
+        <>
+            <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2818.678502474199!2d-93.05779688445632!3d45.05174557909823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa3701502e20c7bd5!2sLisa+Reinhardt+Makeup+and+Hair+Artistry!5e0!3m2!1sen!2sus!4v1534961565240" width="750" height="200" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+        </>
+    );
+}
