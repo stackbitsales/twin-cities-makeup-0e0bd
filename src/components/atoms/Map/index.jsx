1c1ac41fa7861671925cf94c792331053e7244e4
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 23:24:09 +0000

    index.jsx: changed

--- src/components/atoms/Map/index.jsx
+++ src/components/atoms/Map/index.jsx
@@ -1,21 +1,6 @@
 import * as React from 'react';
 
 export default function Map(props) {
-    const { label, altText, url } = props;
-    const icon = props.icon || 'facebook';
-    const IconComponent = iconMap[icon];
-    const annotationPrefix = props['data-sb-field-path'] || '';
-    const annotations = [
-        `${annotationPrefix}`,
-        `${annotationPrefix}.url#@href`,
-        `${annotationPrefix}.altText#@aria-label`,
-        `${annotationPrefix}.elementId#@id`,
-        `${annotationPrefix}.label#span[1]`,
-        `${annotationPrefix}.icon#svg[1]`
-    ];
-    const style = props.style || 'link';
-    const cssClasses = props.className || null;
-    const cssId = props.elementId || null;
 
     return (
         <Link
