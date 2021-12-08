2ca8afb9fa7d12a48975c1504d84816a19119b4c
Stackbit Code Editor <projects@stackbit.com>
2021-12-08 20:36:51 +0000

    index.tsx: changed

--- src/components/sections/MediaGallerySection/index.tsx
+++ src/components/sections/MediaGallerySection/index.tsx
@@ -155,7 +155,7 @@ function MediaGalleryImageGrid(props: MediaGallerySectionProps) {
         return null;
     }
 
-    console.log(columns);
+    console.log(props.columns);
     const columns = props.columns || 4;
     const aspectRatio = props.aspectRatio || '1:1';
     const numGaps = columns - 1; // 1 image, 0 gaps, 2 images, 1 gap, etc etc
