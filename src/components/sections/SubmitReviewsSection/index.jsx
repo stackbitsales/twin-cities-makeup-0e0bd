15bce1b0eda846945e426703a375d39561022e7b
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:02:44 +0000

    index.jsx: changed

--- src/components/sections/SubmitReviewsSection/index.jsx
+++ src/components/sections/SubmitReviewsSection/index.jsx
@@ -4,7 +4,7 @@ export default function SubmitReviewsSection(props) {
 
     return (
         <div className="flex justify-center">
-            <iframe class="mb-3" width="560" height="315" src={iframeLink} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            
         </div>
     );
 }
