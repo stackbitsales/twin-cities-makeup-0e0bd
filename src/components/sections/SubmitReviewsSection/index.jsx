db37d339da6b4f77099bc3c6271267f997bdcc43
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:06:27 +0000

    index.jsx: changed

--- src/components/sections/SubmitReviewsSection/index.jsx
+++ src/components/sections/SubmitReviewsSection/index.jsx
@@ -5,6 +5,7 @@ export default function SubmitReviewsSection(props) {
     return (
         <div className="flex justify-center">
             <p>1submit reviews</p>
+            <p>1submit reviews</p>
         </div>
     );
 }
