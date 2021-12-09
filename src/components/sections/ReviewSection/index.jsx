893b992919d2dd20f641a3d7674f765211e8d81f
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:02:18 +0000

    index.jsx: changed

--- src/components/sections/ReviewSection/index.jsx
+++ src/components/sections/ReviewSection/index.jsx
@@ -2,19 +2,6 @@ import * as React from 'react';
 
 export default function YouTubeSection(props) {
 
-    let youTubeLink = props.url;
-    let youTubeID;
-    let iframeLink;
-
-    if (youTubeLink.includes('youtu.be')) {
-        youTubeID = youTubeLink.split('.be/')[1];
-        youTubeID = youTubeID.split('?')[0];
-    } else {
-        youTubeID = youTubeLink.split('v=')[1];
-    }
-
-    iframeLink = `https://www.youtube-nocookie.com/embed/${youTubeID}`
-
     return (
         <div className="flex justify-center">
             <iframe class="mb-3" width="560" height="315" src={iframeLink} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
