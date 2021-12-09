0588eb4077b4ad6aeb157f30adec5c4fbb2009fc
rylandking <ryland@stackbit.com>
2021-12-09 13:52:03 -0700

    meta data

--- src/components/layouts/BlankBaseLayout/index.tsx
+++ src/components/layouts/BlankBaseLayout/index.tsx
@@ -9,8 +9,13 @@ export default function BlankBaseLayout(props) {
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} data-sb-object-id={pageMeta.id}>
             <Head>
                 <title>{page.title}</title>
-                <meta name="description" content="Stackbit Components Library" />
-                <link rel="icon" href="/favicon.svg" />
+                <meta name="description" content="Premier on-location wedding Makeup and Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more." />
+                <meta property="og:type" content="website"></meta>
+                <meta property="og:title" content="Lisa Reinhardt Makeup &amp; Hair Artistry, LLC | Twincitiesmakeup.com - Twin Cities Bridal Makeup &amp; Hair Artistry"></meta>
+                <meta property="og:description" content="Premier on-location wedding Makeup &amp; Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more."></meta>
+                <meta property="og:url" content="https://www.twincitiesmakeup.com/"></meta>
+                <meta property="og:site_name" content="Lisa Reinhardt Makeup &amp; Hair Artistry, LLC | Twincitiesmakeup.com"></meta>
+                <link rel="icon" href="/favicon-32x32.png" />
             </Head>
             {props.children}
         </div>
--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -14,8 +14,13 @@ export default function DefaultBaseLayout(props) {
             <div className="sb-base sb-default-base-layout">
                 <Head>
                     <title>{page.title}</title>
-                    <meta name="description" content="Stackbit Components Library" />
-                    <link rel="icon" href="/favicon.svg" />
+                    <meta name="description" content="Premier on-location wedding Makeup and Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more." />
+                    <meta property="og:type" content="website"></meta>
+                    <meta property="og:title" content="Lisa Reinhardt Makeup &amp; Hair Artistry, LLC | Twincitiesmakeup.com - Twin Cities Bridal Makeup &amp; Hair Artistry"></meta>
+                    <meta property="og:description" content="Premier on-location wedding Makeup &amp; Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more."></meta>
+                    <meta property="og:url" content="https://www.twincitiesmakeup.com/"></meta>
+                    <meta property="og:site_name" content="Lisa Reinhardt Makeup &amp; Hair Artistry, LLC | Twincitiesmakeup.com"></meta>
+                    <link rel="icon" href="/favicon-32x32.png" />
                 </Head>
                 {site.header && <Header {...site.header} annotationPrefix={siteMeta.id} />}
                 {props.children}
