78c83252c00864ca7fbd1f072fc4eeef4cdd2f1e
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 00:39:41 +0000

    index.jsx: changed

--- src/components/sections/Video/index.jsx
+++ src/components/sections/Video/index.jsx
@@ -14,7 +14,7 @@ export default function VideoSection(props) {
 
     return (
         <div className="flex justify-center">
-            <iframe class="mb-3" width="560" height="315" src="https://www.youtube-nocookie.com/embed/"{youTubeID} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <iframe class="mb-3" width="560" height="315" src="https://www.youtube-nocookie.com/embed/${youTubeID}` frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
     );
 }
