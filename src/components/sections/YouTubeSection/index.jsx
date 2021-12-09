b4b054c47f05f678823603cbfef6382cf8ba3809
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:39:13 +0000

    index.jsx: changed

--- src/components/sections/YouTubeSection/index.jsx
+++ src/components/sections/YouTubeSection/index.jsx
@@ -17,7 +17,7 @@ export default function YouTubeSection(props) {
     iframeLink = `https://www.youtube-nocookie.com/embed/${youTubeID}`
 
     return (
-        <div className="w-full sm:w-11/12 lg:w-3/4 mx-auto mt-5 mb-10 px-6" {...getDataAttrs(props)}>
+        <div className="w-full sm:w-11/12 lg:w-3/4 mx-auto mt-5 mb-10 px-6 md:px-0" {...getDataAttrs(props)}>
             <div className="w-full h-0 relative" style={{ paddingBottom: '56.25%' }}>
                 <iframe className="absolute top-0 left-0 w-full h-full" src={iframeLink} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
