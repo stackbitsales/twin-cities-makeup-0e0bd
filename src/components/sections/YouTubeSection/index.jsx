96e5c1abb4bae3b2c4df649aee95047f66b094e7
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:38:17 +0000

    index.jsx: changed

--- src/components/sections/YouTubeSection/index.jsx
+++ src/components/sections/YouTubeSection/index.jsx
@@ -21,6 +21,6 @@ export default function YouTubeSection(props) {
             <div className="w-full h-0 relative" style={{ paddingBottom: '56.25%' }}>
                 <iframe className="absolute top-0 left-0 w-full h-full" src={iframeLink} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-        </div
+        </div>
     );
 }
