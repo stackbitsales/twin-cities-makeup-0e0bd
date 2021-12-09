5678dbb3060bc7b048879849a719cab827fd7386
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:18:35 +0000

    index.jsx: changed

--- src/components/sections/SubmitReviewsSection/index.jsx
+++ src/components/sections/SubmitReviewsSection/index.jsx
@@ -7,7 +7,7 @@ export default function SubmitReviewsSection(props) {
             <a href="http://www.theknot.com/Vendors/Lisa-Reinhardt-Makeup-and-Hair-Artistry/Profile/HBF/004/333949/Profile?g=0#reviewAdd&utm_source=vendor_website&utm_medium=banner&utm_term=a39a436b-b79f-41d1-bc5a-ce088a58027b&utm_campaign=vendor_badge_assets" target="_blank">
                 <img className="mr-10" src="/images/theknot-logo.jpeg"></img>
             </a>
-            <a href="" target="_blank">
+            <a href="https://www.weddingwire.com/reviews/lisa-reinhardt-makeup-hair-artistry-llc-roseville/954f301ec297f2d0.html" target="_blank">
                 <img className="mr-10" src="/images/weddingwire-logo.jpeg"></img>
             </a>
             <a href="" target="_blank">
