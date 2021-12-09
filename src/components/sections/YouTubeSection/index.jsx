dbd2d7964f7cd6a46c0477a1bd82c5286cd635c9
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:32:17 +0000

    index.jsx: changed

--- src/components/sections/YouTubeSection/index.jsx
+++ src/components/sections/YouTubeSection/index.jsx
@@ -18,7 +18,8 @@ export default function YouTubeSection(props) {
 
     return (
         <div 
-        className="flex justify-center">
+        className="flex justify-center"
+        >
             <iframe class="mb-3" width="560" height="315" src={iframeLink} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
     );
