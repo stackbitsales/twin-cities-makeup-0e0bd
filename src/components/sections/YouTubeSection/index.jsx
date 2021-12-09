a99e5784395471017c89cc824730cccad25303c1
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:32:20 +0000

    index.jsx: changed

--- src/components/sections/YouTubeSection/index.jsx
+++ src/components/sections/YouTubeSection/index.jsx
@@ -17,8 +17,9 @@ export default function YouTubeSection(props) {
     iframeLink = `https://www.youtube-nocookie.com/embed/${youTubeID}`
 
     return (
-        <div 
-        className="flex justify-center"
+        <div
+             
+            className="flex justify-center"
         >
             <iframe class="mb-3" width="560" height="315" src={iframeLink} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
