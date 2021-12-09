c5cad8927225fad35b7f454f0622f6017c53fabf
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:23:12 +0000

    index.jsx: changed

--- src/components/sections/SubmitReviewsSection/index.jsx
+++ src/components/sections/SubmitReviewsSection/index.jsx
@@ -4,7 +4,9 @@ import { getDataAttrs } from '../../../utils/get-data-attrs';
 export default function SubmitReviewsSection(props) {
 
     return (
-        <div className="flex justify-center"
+        <div
+            {...getDataAttrs(props)}
+            className="flex justify-center"
         >
             <a href="http://www.theknot.com/Vendors/Lisa-Reinhardt-Makeup-and-Hair-Artistry/Profile/HBF/004/333949/Profile?g=0#reviewAdd&utm_source=vendor_website&utm_medium=banner&utm_term=a39a436b-b79f-41d1-bc5a-ce088a58027b&utm_campaign=vendor_badge_assets" target="_blank">
                 <img className="mr-10" src="/images/theknot-logo.jpeg"></img>
