38f34349fbd3bcc62d0c2f4195542392e3ba1785
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 00:46:38 +0000

    components-registry.ts: changed

--- src/components/components-registry.ts
+++ src/components/components-registry.ts
@@ -58,6 +58,6 @@ const components = {
     'PageLayout': dynamic(() => import('./layouts/PageLayout')),
     'PostLayout': dynamic(() => import('./layouts/PostLayout')),
     'PostFeedLayout': dynamic(() => import('./layouts/PostFeedLayout')),
-    'PostFeedCategoryLayout': dynamic(() => import('./layouts/PostFeedCategoryLayout'))
+    'PostFeedCategoryLayout': dynamic(() => import('./layouts/PostFeedCategoryLayout')),
     'YouTubeSection': dynamic(() => import('./sections/YouTubeSection'))
 };
