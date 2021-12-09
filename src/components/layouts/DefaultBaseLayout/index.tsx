5786f73bf85d0c5ada22b460144e98d6d1c49fa8
rylandking <ryland@stackbit.com>
2021-12-09 13:59:03 -0700

    SEO optimization

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -13,8 +13,8 @@ export default function DefaultBaseLayout(props) {
         <div className={classNames('sb-page', pageMeta.pageCssClasses)} data-sb-object-id={pageMeta.id}>
             <div className="sb-base sb-default-base-layout">
                 <Head>
-                    <title>{page.title}</title>
-                    <meta name="description" content="Premier on-location wedding Makeup and Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more." />
+                    <title>{page.title} - Twin Cities Makeup &amp; Hair Services</title>
+                    <meta name="description" content="Premier on-location wedding Makeup &amp; Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more." />
                     <meta property="og:type" content="website"></meta>
                     <meta property="og:title" content="Lisa Reinhardt Makeup &amp; Hair Artistry, LLC | Twincitiesmakeup.com - Twin Cities Bridal Makeup &amp; Hair Artistry"></meta>
                     <meta property="og:description" content="Premier on-location wedding Makeup &amp; Hair Service for Minneapolis and St. Paul. Bridal hair and makeup, NovaLash Eyelash Extensions, airbrush makeup, and more."></meta>
