96677247b0a282c0ba22cfdc75505104af9d4bad
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:37:57 +0000

    index.jsx: changed

--- src/components/sections/YouTubeSection/index.jsx
+++ src/components/sections/YouTubeSection/index.jsx
@@ -25,7 +25,7 @@ export default function YouTubeSection(props) {
         </div>
         <div className="w-full sm:w-11/12 lg:w-3/4 mx-auto mt-5 mb-10" {...getDataAttrs(props)}>
             <div className="w-full h-0 relative" style={{ paddingBottom: '56.25%' }} data-sb-field-path={annotationPrefix}>
-                <iframe className="absolute top-0 left-0 w-full h-full" src={`https://www.youtube-nocookie.com/embed/${videoId}`} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className="absolute top-0 left-0 w-full h-full" src={iframeLink} title={videoTitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div
     );
