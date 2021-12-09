19a3361ad336c2f994ccf0f5b2fe65c73edcfdf0
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:18:25 +0000

    index.jsx: changed

--- src/components/sections/SubmitReviewsSection/index.jsx
+++ src/components/sections/SubmitReviewsSection/index.jsx
@@ -4,7 +4,7 @@ export default function SubmitReviewsSection(props) {
 
     return (
         <div className="flex justify-center">
-            <a href="" target="_blank">
+            <a href="http://www.theknot.com/Vendors/Lisa-Reinhardt-Makeup-and-Hair-Artistry/Profile/HBF/004/333949/Profile?g=0#reviewAdd&utm_source=vendor_website&utm_medium=banner&utm_term=a39a436b-b79f-41d1-bc5a-ce088a58027b&utm_campaign=vendor_badge_assets" target="_blank">
                 <img className="mr-10" src="/images/theknot-logo.jpeg"></img>
             </a>
             <a href="" target="_blank">
