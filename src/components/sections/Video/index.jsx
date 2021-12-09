28ec4bf340d8883f83a03c62e64dcabe05fa442d
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 00:35:16 +0000

    index.jsx: changed

--- src/components/sections/Video/index.jsx
+++ src/components/sections/Video/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default function MapSection(props) {
+export default function VideoSection(props) {
 
     return (
         <div className="flex justify-center">
