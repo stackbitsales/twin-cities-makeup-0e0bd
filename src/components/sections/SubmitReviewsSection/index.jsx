e14ca3255ec62e1618a5d75b05064e3debc85141
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 15:17:42 +0000

    index.jsx: changed

--- src/components/sections/SubmitReviewsSection/index.jsx
+++ src/components/sections/SubmitReviewsSection/index.jsx
@@ -4,6 +4,7 @@ export default function SubmitReviewsSection(props) {
 
     return (
         <div className="flex justify-center">
+            <a href=""
             <img className="mr-10" src="/images/theknot-logo.jpeg"></img>
             <img className="mr-10" src="/images/weddingwire-logo.jpeg"></img>
             <img className="" src="/images/facebook-logo.jpeg"></img>
