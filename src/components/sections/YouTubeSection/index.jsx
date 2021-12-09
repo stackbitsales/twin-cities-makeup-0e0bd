fd65d8b597701ad6cb92889570541053455b0a14
Stackbit Code Editor <projects@stackbit.com>
2021-12-09 00:42:10 +0000

    YouTubeSection: added

---
+++ src/components/sections/YouTubeSection/index.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export default function VideoSection(props) {
+
+    let videoLink;
+    let youTubeID;
+
+    if (videoLink.includes('youtu.be')) {
+        youTubeID = videoLink.split('.be/')[1];
+        youTubeID = youTubeID.split('?')[0];
+    } else {
+        youTubeID = videoLink.split('v=')[1];
+    }
+
+    return (
+        <div className="flex justify-center">
+            <iframe class="mb-3" width="560" height="315" src="https://www.youtube-nocookie.com/embed/"{youTubeID} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+    );
+}
